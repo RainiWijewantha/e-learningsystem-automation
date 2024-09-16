0e827271591d506392f4c0e6b26e824af931a9f9
Raini Wijewantha <sashee.wijewantha@gmail.com>
2024-09-17 03:59:55 +0530

    admin updated

---
+++ Automation/src/Admin/AdminLesson.java
@@ -0,0 +1,46 @@
+package Admin;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class AdminLesson {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stubSystem.setProperty("webdriver.chrome.driver", "E:\\ICBT - BSc\\Final Project\\e-learningsystem-automation\\Chrome Driver\\chromedriver-win64\\chromedriver.exe");
+
+		System.setProperty("webdriver.chrome.driver", "E:\\ICBT - BSc\\Final Project\\e-learningsystem-automation\\Chrome Driver\\chromedriver-win64\\chromedriver.exe");
+
+		ChromeOptions option = new ChromeOptions();
+		option.addArguments("--remote-allow-origins=*");
+
+		WebDriver chromeDriver = new ChromeDriver(option);
+		chromeDriver.manage().window().maximize();
+
+		String baseURL = "http://localhost:8080/e-learningsystem/admin/login.php";
+		chromeDriver.get(baseURL);
+		Thread.sleep(5000);
+
+		WebDriverWait wait = new WebDriverWait(chromeDriver, Duration.ofSeconds(2000));
+		
+		//login
+		WebElement username3 = chromeDriver.findElement(By.xpath("//input[@id='email']"));
+		username3.sendKeys("Admin");
+		Thread.sleep(5000);
+
+		WebElement password3 = chromeDriver.findElement(By.xpath("//input[@id='password']"));
+		password3.sendKeys("admin");
+		Thread.sleep(5000);
+
+		WebElement login_btn3 = chromeDriver.findElement(By.xpath("//button[normalize-space()='Log In']"));
+		login_btn3.click();
+		Thread.sleep(5000);
+
+	}
+
+}
