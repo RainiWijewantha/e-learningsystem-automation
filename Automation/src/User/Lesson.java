782136cae604bdf594545f47086f6854e2955648
Raini Wijewantha <sashee.wijewantha@gmail.com>
2024-09-16 22:58:44 +0530

    Lesson.java

---
+++ Automation/src/User/Lesson.java
@@ -0,0 +1,56 @@
+package User;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Lesson {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "E:\\ICBT - BSc\\Final Project\\e-learningsystem-automation\\Chrome Driver\\chromedriver-win64\\chromedriver.exe");
+
+		ChromeOptions option = new ChromeOptions();
+		option.addArguments("--remote-allow-origins=*");
+
+		WebDriver chromeDriver = new ChromeDriver(option);
+		chromeDriver.manage().window().maximize();
+
+		String baseURL = "http://localhost:8080/e-learningsystem/login.php";
+		chromeDriver.get(baseURL);
+		Thread.sleep(5000);
+
+		WebDriverWait wait = new WebDriverWait(chromeDriver, Duration.ofSeconds(2000));
+		
+		//login
+		WebElement username3 = chromeDriver.findElement(By.xpath("//input[@id='email']"));
+		username3.sendKeys("RainiWijewantha");
+		Thread.sleep(5000);
+		
+		WebElement password3 = chromeDriver.findElement(By.xpath("//input[@id='password']"));
+		password3.sendKeys("123");
+		Thread.sleep(5000);
+		
+		WebElement login_btn3 = chromeDriver.findElement(By.xpath("//button[normalize-space()='Log In']"));
+		login_btn3.click();
+		Thread.sleep(5000);
+		
+		//lesson tab
+		WebElement lesson = chromeDriver.findElement(By.xpath("//a[normalize-space()='Lesson']"));
+		lesson.click();
+		Thread.sleep(5000);
+		
+		//view file
+		WebElement view_file = chromeDriver.findElement(By.xpath("(//a[@class='btn btn-xs btn-info'][normalize-space()='View File'])[2]"));
+		view_file.click();
+		Thread.sleep(5000);
+
+	}
+
+}
